67ebc7d1c180dd32abc1bb051f00e4b6689fe549
吴祥熠 <1054353861@qq.com>
2018-07-17 16:29:15 +0800

    添加了圆角矩形demo

---
+++ app/src/main/java/com/android/round/TestActivity.java
@@ -0,0 +1,15 @@
+package com.android.round;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android.demo.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
